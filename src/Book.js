963323abc3d447fb3b903844f7aba9a77e3ce07e
Alessia <alessandri.alessia@gmail.com>
2018-07-28 12:02:00 +0200

    changed the way images are passed

--- src/Book.js
+++ src/Book.js
@@ -24,7 +24,8 @@ class Book extends Component {
    return(
      <div className="book">
        <div className="book-top">
-         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${ thumbnail }")` }}>
+         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks ?
+                thumbnail : ''})` }}>
          </div>
          <div className="book-shelf-changer">
              <select value={shelf} onChange={(event) => this.updateShelf(event.target.value)}>
